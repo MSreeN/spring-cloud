dda51982c21c9bdfb29822f759899edf77bb0b2a
MSreeN <khyathimnsree369@gmail.com>
2024-09-13 22:14:34 +0530

    defined a sample route

--- gatewayserver/src/main/java/com/learn/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/learn/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,25 @@ package com.learn.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+
+import java.time.LocalTime;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	public RouteLocator gatewayConfig(RouteLocatorBuilder routeLocatorBuilder){
+		return routeLocatorBuilder.routes()
+				.route(r -> r.path("/gateway/accounts/**")
+						.filters(f -> f.rewritePath("/gateway/accounts/(?<remaining>.*)",
+										"/${remaining}")
+								.addRequestHeader("time", LocalTime.now().toString()))
+						.uri("lb://ACCOUNTS")).build();
 	}
 
 }
