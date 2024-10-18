765db3e9fabd4cdcddccb10304f6348efaf40bb8
MSreeN <khyathimnsree369@gmail.com>
2024-10-18 23:16:21 +0530

    created a custom circuit breaker

---
+++ gatewayserver/src/main/java/com/learn/gatewayserver/config/CircuitBreakerConfig.java
@@ -0,0 +1,27 @@
+package com.learn.gatewayserver.config;
+
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class CircuitBreakerConfig {
+
+    @Bean
+    public CircuitBreaker circuitBreakerConfig(){
+        io.github.resilience4j.circuitbreaker.CircuitBreakerConfig circuitBreaker = io.github.resilience4j.circuitbreaker.CircuitBreakerConfig.custom()
+                .slidingWindowSize(10)
+                .failureRateThreshold(50)
+                .waitDurationInOpenState(Duration.ofMillis(10000))
+                .permittedNumberOfCallsInHalfOpenState(2)
+                .minimumNumberOfCalls(0)
+                .build();
+
+        CircuitBreakerRegistry registry = CircuitBreakerRegistry.of(circuitBreaker);
+        return registry.circuitBreaker("custom");
+    }
+}
