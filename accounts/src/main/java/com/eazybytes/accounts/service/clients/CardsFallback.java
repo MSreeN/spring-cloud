3ff4cd362111dc8a0b2d58d2b3c89a9a76c149f1
MSreeN <khyathimnsree369@gmail.com>
2024-10-22 23:54:24 +0530

    implemented circuit breaker in accounts

---
+++ accounts/src/main/java/com/eazybytes/accounts/service/clients/CardsFallback.java
@@ -0,0 +1,13 @@
+package com.eazybytes.accounts.service.clients;
+
+import com.eazybytes.accounts.dto.CardsDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CardsFallback implements CardsFeignClient{
+    @Override
+    public ResponseEntity<CardsDto> fetchCardDetails(String mobileNumber) {
+        return null;
+    }
+}
